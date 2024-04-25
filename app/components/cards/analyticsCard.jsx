b6fdf1c536f80b7c3afe0cbbed81770c5e02055f
Jhon-crypt <johnoladele690@gmail.com>
2024-04-26 00:50:16 +0100

    feat:Added hover to border cards

--- app/components/cards/analyticsCard.jsx
+++ app/components/cards/analyticsCard.jsx
@@ -4,7 +4,7 @@ export default function AnalyticsCard() {
 
         <>
 
-            <div className="card m-0" style={{ height: "185px", backgroundColor: "transparent", borderRadius: "10px", border: "2px solid #E0D9DE" }}>
+            <div className="card m-0" id="dashnav" style={{ height: "185px", backgroundColor: "transparent", borderRadius: "10px" }}>
                 <div className="card-body">
                     <div className="mt-4 mx-auto d-flex justify-content-center align-items-center" style={{ backgroundColor: "#EBDEEA", width: '70px', height: '70px', borderRadius: '50%' }}>
                         <i className="bi bi-graph-up-arrow" style={{ color: "#E84D88", fontSize: "25px" }} />
