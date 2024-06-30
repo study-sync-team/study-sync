fbea0630a3226daff408a46666a4fac7df022fce
Jhon-crypt <johnoladele690@gmail.com>
2024-06-30 01:33:08 +0100

    added waitlist

--- app/signup/waitlist/page.js
+++ app/signup/waitlist/page.js
@@ -1,12 +1,30 @@
+"use client"
+import DesktopWarning from "@/app/components/section/desktopWarning"
+import { useMediaQuery } from 'react-responsive'
 import WaitlistSection from "@/app/components/section/waitlistSection"
 
-export default function Page(){
+export default function Page() {
+
+    const Desktop = ({ children }) => {
+        const isDesktop = useMediaQuery({ minWidth: 992 })
+        return isDesktop ? children : null
+    }
+
+    const Mobile = ({ children }) => {
+        const isMobile = useMediaQuery({ maxWidth: 767 })
+        return isMobile ? children : null
+    }
 
     return (
 
         <>
+            <Desktop>
+                <DesktopWarning />
+            </Desktop>
+            <Mobile>
+                <WaitlistSection />
+            </Mobile>
 
-            <WaitlistSection />
 
         </>
 
